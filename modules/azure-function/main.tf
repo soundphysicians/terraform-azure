efa86ffa41b2d9bde9f3b3f6afb5e18d44e864b7
Steve Bering <sbering@soundphysicians.com>
2024-02-29 17:31:52 -0800

    update function app to azurerm_linux_function_app

--- modules/azure-function/main.tf
+++ modules/azure-function/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.0"
     }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = ">= 1.2.11"
-    }
   }
 }
 
@@ -17,17 +13,33 @@ terraform {
 data "azurerm_client_config" "current" {}
 data "azuread_client_config" "current" {}
 
+locals {
+  # Lowercase values for use in resource names
+  environment = lower(var.environment)
+  prefix      = lower(var.prefix)
+
+  tags = {
+    environment = local.environment
+    project     = local.prefix
+  }
+
+  # Pass appsettings from variable with embedded defaults
+  app_settings = merge({
+    APPINSIGHTS_INSTRUMENTATIONKEY = var.app_insights_key
+    AZURE_FUNCTIONS_ENVIRONMENT    = var.function_environment
+    FUNCTIONS_WORKER_RUNTIME       = "dotnet"
+  }, coalesce(var.app_settings, {}))
+
+  function_name        = coalesce(var.function_name, "${local.prefix}-${local.environment}-fn-${var.base_name}")
+  storage_account_name = lower(coalesce(var.function_name, "${local.prefix}${local.environment}sa${var.base_name}"))
+  plan_name            = coalesce(var.plan_name, "${local.prefix}-${local.environment}-asplan-${var.base_name}")
+}
+
 # ------------------------------------------------------------------------
 # Storage Account for Function App
 # ------------------------------------------------------------------------
-resource "azurecaf_name" "storage" {
-  resource_type = "azurerm_storage_account"
-  prefixes      = [var.prefix, var.environment]
-  suffixes      = [var.base_name]
-}
-
 resource "azurerm_storage_account" "storage" {
-  name                     = azurecaf_name.storage.result
+  name                     = local.storage_account_name
   location                 = var.location
   resource_group_name      = var.resource_group_name
   account_kind             = "StorageV2"
@@ -37,60 +49,33 @@ resource "azurerm_storage_account" "storage" {
 
   enable_https_traffic_only = "true"
 
-  tags = {
-    environment = var.environment
-    project     = var.prefix
-  }
+  tags = local.tags
 }
 
 # ------------------------------------------------------------------------
 # Service Plan
 # ------------------------------------------------------------------------
-resource "azurecaf_name" "plan" {
-  resource_type = "azurerm_app_service_plan"
-  prefixes      = [var.prefix, var.environment]
-  suffixes      = [var.base_name]
-}
-
-resource "azurerm_app_service_plan" "plan" {
-  name                = azurecaf_name.plan.result
+resource "azurerm_service_plan" "plan" {
+  name                = local.plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "FunctionApp"
+  os_type             = "Linux"
+  sku_name            = "Y1"
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-
-  tags = {
-    environment = var.environment
-    project     = var.prefix
-  }
-}
-
-locals {
-  # Pass appsettings from variable with embedded defaults
-  app_settings = merge({
-    APPINSIGHTS_INSTRUMENTATIONKEY = var.app_insights_key
-    AZURE_FUNCTIONS_ENVIRONMENT    = var.function_environment
-    FUNCTIONS_WORKER_RUNTIME       = "dotnet"
-  }, coalesce(var.app_settings, {}))
-
-  env_supports_slots = contains(["prd", "tst", "stage"], var.environment) ? true : false
+  tags = local.tags
 }
 
 # ------------------------------------------------------------------------
 # Azure AD Application Registration
 # ------------------------------------------------------------------------
 resource "azuread_application" "app" {
-  display_name                   = azurecaf_name.fn.result
+  display_name                   = local.function_name
   group_membership_claims        = ["None"]
   owners                         = [data.azuread_client_config.current.object_id]
   sign_in_audience               = "AzureADMyOrg"
   prevent_duplicate_names        = false # prevent lookups that we may not have rights to make
   fallback_public_client_enabled = false
-  tags                           = [var.environment, var.prefix]
+  tags                           = values(local.tags)
 
   api {
     mapped_claims_enabled          = true
@@ -124,12 +109,12 @@ resource "azuread_application" "app" {
 # Function App Service Principal
 # ------------------------------------------------------------------------
 resource "azuread_service_principal" "app" {
-  application_id               = azuread_application.app.application_id
+  client_id                    = azuread_application.app.client_id
   app_role_assignment_required = false
   alternative_names            = []
   notification_email_addresses = []
   owners                       = [data.azuread_client_config.current.object_id]
-  tags                         = [var.environment, var.prefix]
+  tags                         = values(local.tags)
 
   lifecycle {
     ignore_changes = [
@@ -139,45 +124,53 @@ resource "azuread_service_principal" "app" {
 }
 
 # ------------------------------------------------------------------------
-# Function App
+# User assigned identity for function
 # ------------------------------------------------------------------------
-resource "azurecaf_name" "fn" {
-  resource_type = "azurerm_function_app"
-  prefixes      = [var.prefix, var.environment]
-  suffixes      = [var.base_name]
+resource "azurerm_user_assigned_identity" "app" {
+  name                = local.function_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = local.tags
 }
 
-resource "azurerm_function_app" "app" {
-  name                = azurecaf_name.fn.result
-  location            = var.location
+# ------------------------------------------------------------------------
+# Function App
+# ------------------------------------------------------------------------
+resource "azurerm_linux_function_app" "app" {
+  name                = local.function_name
   resource_group_name = var.resource_group_name
-  version             = "~4"
+  location            = var.location
   https_only          = true
+
+  functions_extension_version     = "~4"
+  public_network_access_enabled   = true
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.app.principal_id
+
   site_config {
-    app_scale_limit = 1
+    app_scale_limit = var.app_scale_limit
     http2_enabled   = true
-    min_tls_version = 1.2
+
+    remote_debugging_enabled = var.remote_debugging_enabled
+    remote_debugging_version = "VS2022"
+
+    runtime_scale_monitoring_enabled = true
   }
 
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  service_plan_id            = azurerm_service_plan.plan.id
 
   # Pass appsettings from variable with embedded defaults
   app_settings = local.app_settings
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.app.id]
   }
 
-  # TODO: re-enable auth once we get this into test without authn/authz enabled
   auth_settings {
     enabled = false
-    active_directory {
-      client_id         = azuread_application.app.application_id
-      allowed_audiences = []
-    }
-    default_provider = "AzureActiveDirectory"
   }
 
   lifecycle {
@@ -191,49 +184,43 @@ resource "azurerm_function_app" "app" {
 
   depends_on = [
     azurerm_storage_account.storage,
-    azurerm_app_service_plan.plan
+    azurerm_service_plan.plan
   ]
 
-  tags = {
-    environment = var.environment
-    project     = var.prefix
-  }
+  tags = local.tags
 }
 
 # ------------------------------------------------------------------------
 # Setup slot for function
 # ------------------------------------------------------------------------
-resource "azurerm_function_app_slot" "slot" {
-  count               = local.env_supports_slots ? 1 : 0
-  function_app_name   = azurerm_function_app.app.name
-  name                = "staging"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  version             = "~4"
-  https_only          = true
+resource "azurerm_linux_function_app_slot" "slot" {
+  count           = var.deploy_using_slots ? 1 : 0
+  function_app_id = azurerm_linux_function_app.app.id
+  name            = "staging"
+  https_only      = true
   site_config {
-    app_scale_limit = 1
+    app_scale_limit = var.app_scale_limit
     http2_enabled   = true
-    min_tls_version = 1.2
+
+    remote_debugging_enabled = var.remote_debugging_enabled
+    remote_debugging_version = "VS2022"
+
+    runtime_scale_monitoring_enabled = true
   }
 
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  service_plan_id            = azurerm_service_plan.plan.id
 
   app_settings = local.app_settings
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.app.id]
   }
 
   auth_settings {
     enabled = false
-    active_directory {
-      client_id         = azuread_application.app.application_id
-      allowed_audiences = []
-    }
-    default_provider = "AzureActiveDirectory"
   }
 
   lifecycle {
@@ -247,19 +234,17 @@ resource "azurerm_function_app_slot" "slot" {
 
   depends_on = [
     azurerm_storage_account.storage,
-    azurerm_app_service_plan.plan
+    azurerm_service_plan.plan
   ]
 
-  tags = {
-    environment = var.environment
-    project     = var.prefix
-  }
+  tags = local.tags
 }
 
 # ------------------------------------------------------------------------
 # KeyVault for secrets
 # ------------------------------------------------------------------------
 data "azurerm_key_vault" "key_vault" {
+  count               = var.key_vault != null ? 1 : 0
   name                = var.key_vault.name
   resource_group_name = var.key_vault.resource_group_name
 }
@@ -268,9 +253,10 @@ data "azurerm_key_vault" "key_vault" {
 # Grant access to the key vault for the primary function
 # ------------------------------------------------------------------------
 resource "azurerm_key_vault_access_policy" "app" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  count        = var.key_vault != null ? 1 : 0
+  key_vault_id = data.azurerm_key_vault.key_vault[0].id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.app.identity[0].principal_id
+  object_id    = azurerm_user_assigned_identity.app.principal_id
 
   secret_permissions = [
     "Get",
@@ -282,10 +268,10 @@ resource "azurerm_key_vault_access_policy" "app" {
 # Grant access to the key vault for the slot function, if we have one
 # ------------------------------------------------------------------------
 resource "azurerm_key_vault_access_policy" "slot" {
-  count        = local.env_supports_slots ? 1 : 0
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  count        = var.key_vault != null ? length(azurerm_linux_function_app_slot.slot) : 0
+  key_vault_id = data.azurerm_key_vault.key_vault[0].id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app_slot.slot[0].identity[0].principal_id
+  object_id    = azurerm_user_assigned_identity.app.principal_id
 
   secret_permissions = [
     "Get",
--- modules/azure-function/variables.tf
+++ modules/azure-function/variables.tf
@@ -1,14 +1,26 @@
 # Prefix Applied to  All Resource Names
-variable "prefix" {}
+variable "prefix" {
+  type        = string
+  description = "Prefix to apply to all resource names"
+}
 
 # Name of the environment to deploy to
-variable "environment" {}
+variable "environment" {
+  type        = string
+  description = "Name of the environment to deploy to"
+}
 
 # Location Where All Resources Will Be Deployed
-variable "location" {}
+variable "location" {
+  type        = string
+  description = "Location to deploy resources to"
+}
 
 # Resource Group Where All Resources Will Be Deployed
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group to place resources in"
+}
 
 # Base Name for All Resources 
 # Given: 
@@ -19,16 +31,77 @@ variable "resource_group_name" {}
 #   function name:      sbs-tst-fn-myfunc
 #   storage account:    sbststsamyfunc
 #   service plan:       sbs-tst-asplan-myfunc
-variable "base_name" {}
+variable "base_name" {
+  type        = string
+  description = "Discriminator name for all resources"
+}
+
+variable "function_name" {
+  type        = string
+  description = "Name of the function app to deploy"
+  default     = null
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account to use for the function app"
+  default     = null
+}
+
+variable "plan_name" {
+  type        = string
+  description = "Name of the service plan to use for the function app"
+  default     = null
+}
 
 # Instrumentation Key for AppInsights instance
-variable "app_insights_key" {}
+variable "app_insights_key" {
+  type        = string
+  description = "Instrumentation key for the Application Insights instance to use for logging"
+  default     = ""
+}
 
 # Function environment to deploy to (Development, Staging, Production)
-variable "function_environment" {}
+variable "function_environment" {
+  type        = string
+  description = "Prefix of the environment to deploy the function app to"
+  default     = "Development"
+}
+
+variable "dotnet_version" {
+  type        = string
+  description = "Version of the .NET runtime to use for the function app"
+  default     = "v6.0"
+
+  validation {
+    condition     = can(regex("^(v\\d+\\.\\d+)$", var.dotnet_version))
+    error_message = "Dotnet version must be in the format 'vX.X'"
+  }
+
+  validation {
+    condition     = contains(["v4.0", "v6.0", "v7.0", "v8.0"], var.dotnet_version)
+    error_message = "Dotnet version must one of the following values (v4.0, v6.0, v7.0 or v8.0)"
+  }
+}
+
+variable "app_scale_limit" {
+  type        = number
+  description = "Maximum number of instances to scale the function app to"
+  default     = 1
+}
+
+variable "remote_debugging_enabled" {
+  type        = bool
+  description = "Whether or not to enable remote debugging for the function app"
+  default     = false
+}
 
 # Map of application settings to send to the function app
-variable "app_settings" {}
+variable "app_settings" {
+  type        = map(string)
+  description = "Key value pairs of application settings to apply to the function application"
+  default     = {}
+}
 
 variable "app_roles" {
   type = list(object({
@@ -38,6 +111,7 @@ variable "app_roles" {
     description : string
   }))
   description = "List of Application scoped roles to support"
+  default     = []
 }
 
 variable "key_vault" {
@@ -46,4 +120,11 @@ variable "key_vault" {
     name                = string,
     resource_group_name = string
   })
+  default = null
+}
+
+variable "deploy_using_slots" {
+  description = "Whether or not to deploy the function app using deployment slots. If true, a staging slot will be created."
+  type        = bool
+  default     = false
 }
