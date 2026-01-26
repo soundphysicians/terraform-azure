bc69382c5eea3cf366711ff3010727971b89dc34
Steve Bering <sbering@soundphysicians.com>
2026-01-26 08:26:53 -0800

    feature: add sku_name variable for configurable linux function app SKU

--- modules/azure-linux-function/main.tf
+++ modules/azure-linux-function/main.tf
@@ -65,7 +65,7 @@ resource "azurerm_service_plan" "plan" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = var.sku_name
 
   tags = local.tags
 }
